0565da123ff55ff48469ff607832c0d6659b1478
DayKui <15658052167@163.com>
2019-01-29 00:05:25 +0800

    继续学习编辑器知识
    继续学习编辑器知识

---
+++ Assets/YuSongStudy/Editor/DontForce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class DontForce {
+    [InitializeOnLoadMethod]
+    static void InitializeOnLoadMethod()
+    {
+        SceneView.onSceneGUIDelegate = delegate (SceneView sceneView)
+        {
+            Event e = Event.current;
+            if (e != null)
+            {
+                int controlId = GUIUtility.GetControlID(FocusType.Passive);
+                if (e.type == EventType.Layout)
+                {
+                    HandleUtility.AddDefaultControl(controlId);
+                }
+            }
+        };
+    }
+}
